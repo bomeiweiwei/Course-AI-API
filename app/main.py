cf0f786a4625cba64cf1d653689731935e084fbb
bomeiweiwei <bomeilonch0210@gmail.com>
2026-05-08 23:43:46 +0800

    新增docker

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.core.config import get_settings
 from app.routers import courses, recommend, ai
 from app.routers import options
+from fastapi.middleware.cors import CORSMiddleware
 
 settings = get_settings()
 
@@ -11,6 +12,21 @@ app.include_router(courses.router, prefix="/api/courses", tags=["Courses"])
 app.include_router(recommend.router, prefix="/api/recommend", tags=["Recommend"])
 app.include_router(ai.router, prefix="/api/ai", tags=["AI"])
 app.include_router(options.router, prefix="/api/options", tags=["Options"])
+
+origins = [
+    "http://localhost:8501",
+    "http://127.0.0.1:8501",
+    # 之後部署後再加：
+    # "https://你的-streamlit-cloud-run-url.run.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
